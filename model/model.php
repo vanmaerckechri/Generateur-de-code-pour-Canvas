be630bb83447215f85c0a39cebe088d0980df167
vanmaerckechri <vanmaerckechri@gmail.com>
2018-03-23 13:53:39 +0100

    base du crud ok

--- model/model.php
+++ model/model.php
@@ -79,16 +79,17 @@ class Crud
 				}
 			}
 
-			$prepareDyn = substr($prepareDyn, 0, strlen($prepareDyn) - 2);
+			$prepareDyn = $typeReq != "DELETE" ? substr($prepareDyn, 0, strlen($prepareDyn) - 2) : $prepareDyn;
 			$prepareDyn .= " ";
 			$prepareDyn .= $typeReq == "INSERT INTO" ?  ") " : " ";
-			$prepareDyn .= $typeReq == "SELECT" ? "FROM " : "";
-			$prepareDyn .= $typeReq == "SELECT" ?  $this->_table : "";
+			$prepareDyn .= $typeReq == "SELECT" || $typeReq == "DELETE" ? "FROM " : "";
+			$prepareDyn .= $typeReq == "SELECT" || $typeReq == "DELETE" ?  $this->_table : "";
+
 			$prepareDyn .= $typeReq != "INSERT INTO" ? " WHERE ": "";
 			$prepareDyn .= $typeReq == "INSERT INTO" ? "VALUES " : "";
 			$prepareDyn .= $typeReq == "INSERT INTO" ?  "( " : "";
 
-			if ($typeReq == "SELECT" || $typeReq == "UPDATE")
+			if ($typeReq == "SELECT" || $typeReq == "UPDATE" || $typeReq == "DELETE")
 			{
 				$arrayMultiLength = array_sum(array_map("count", $where));
 				$index = 1;
@@ -96,10 +97,10 @@ class Crud
 				{
 					foreach ($title as $key2 => $value)
 					{
-						$prepareDyn .= $typeReq != "INSERT INTO" ? $key1." = :".$key1.$index : "";
+						$prepareDyn .= $key1." = :".$key1.$index;
 						if ($index < $arrayMultiLength)
 						{
-							$prepareDyn .= $typeReq != "INSERT INTO" ? " OR " : "";
+							$prepareDyn .= " OR ";
 						}
 						$index++;
 					}
@@ -118,7 +119,7 @@ class Crud
 			$req = $this->_db->prepare($prepareDyn);
 			echo $prepareDyn;
 			//BINDPARAM.
-			if ($typeReq == "SELECT")
+			if ($typeReq == "SELECT" || $typeReq == "DELETE")
 			{
 				$index = 1;
 				foreach ($where as $key1 => $title)
@@ -128,6 +129,7 @@ class Crud
 						$focus = ":".$key1;
 						$focus .= $index;
 						$req->bindValue($focus, $value);
+						echo '$req->bindValue('.$focus.', '.$value.')';
 						$index++;
 					}
 				}
@@ -156,7 +158,7 @@ class Crud
 		}
 	}
 
-	public function selectCustom($columns, $whereDyn)
+	public function select($columns, $whereDyn)
 	{
 		if (isset($columns) && isset($whereDyn))
 		{
@@ -170,7 +172,7 @@ class Crud
 		}
 	}
 
-	public function insertCustom($columns, $whereDyn)
+	public function insert($columns, $whereDyn)
 	{
 		if (isset($columns) && isset($whereDyn))
 		{
@@ -182,11 +184,23 @@ class Crud
 		}
 	}
 
-	public function updateCustom($columns, $whereDyn)
+	public function update($columns, $whereDyn)
 	{
 		if (isset($columns) && isset($whereDyn))
 		{
 			$req = $this->prepare($columns, $whereDyn, 'UPDATE');
+			$req->execute();
+
+		   	$req->closeCursor();
+		    $req = NULL;
+		}
+	}
+
+	public function delete($columns, $whereDyn)
+	{
+		if (isset($columns) && isset($whereDyn))
+		{
+			$req = $this->prepare($columns, $whereDyn, 'DELETE');
 			$req->execute();
 
 		   	$req->closeCursor();
@@ -209,15 +223,19 @@ $test = new Crud($dbCoordinates);
 //FROM ou équivalent de la requete => '$columns': valeur = nom colonne.
 //WHERE ou équivalent de la de la requete => '$whereDyn': nom array enfant = nom colonne. valeur(s) de l'array enfant = contenu colonne.
 	//exemple de requete SELECT.
-		$columns = array ("login", "password", "mail");
-		$whereDyn = array ("id" => array(1, 3));
-		$members = $test->selectCustom($columns, $whereDyn);
+		/*$columns = array ("login", "password", "mail");
+		$whereDyn = array ("id" => array(2, 5));
+		$members = $test->select($columns, $whereDyn);*/
 	//exemple de requete INSERT.
 		/*$columns = array ("login" => array("testlogin"), "password" => array("testpsw"), "mail" => array("testmail"));
 		$whereDyn = array();
-		$members = $test->insertCustom($columns, $whereDyn);*/
+		$members = $test->insert($columns, $whereDyn);*/
 	//exemple de requete UPDATE.
 		/*$columns = array ("login" => array("ttt"), "password" => array("ppp"), "mail" => array("mmm"));
+		$whereDyn = array ("id" => array(2, 5));
+		$members = $test->update($columns, $whereDyn);*/
+	//exemple de requete DELETE.
+		/*$columns = array ();
 		$whereDyn = array ("id" => array(1, 3));
-		$members = $test->updateCustom($columns, $whereDyn);*/
+		$members = $test->delete($columns, $whereDyn);*/
 	var_dump($members);
