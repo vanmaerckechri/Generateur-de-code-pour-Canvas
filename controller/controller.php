4acec52c8a59a8b1ee04fe0c44c56035ef62a1ec
vanmaerckechri <vanmaerckechri@gmail.com>
2018-03-26 16:24:01 +0200

    update echo sessionAuth

--- controller/controller.php
+++ controller/controller.php
@@ -4,8 +4,10 @@ require('./model/model.php');
 //Récupérer les valeurs 'login' et 'pwd' de session
 $auth = new Authentification();
 $sessionLoginInfo = $auth->updateCheckSession();
+require('humhum.php');
+
 //Utiliser les informations de session en condition lors d'une requête 'select' pour voir si le membre existe.
-$dbCoordinates = ["dbHost" => "localhost", "dbPort" => "", "dbName" => "gen_code_canvas", "dbCharset" => "utf8", "dbLogin" => "root", "dbPwd" => "", "table" => "members"];
+/*$dbCoordinates = ["dbHost" => "localhost", "dbPort" => "", "dbName" => "gen_code_canvas", "dbCharset" => "utf8", "dbLogin" => "root", "dbPwd" => "", "table" => "members"];*/
 $crud = new Crud($dbCoordinates);
 $columns = array ("id");
 $whereDyn = array ("login" => array($sessionLoginInfo['login']), "password" => array($sessionLoginInfo['password']));
@@ -13,8 +15,6 @@ $operator = "AND";
 $memberExist = $crud->select($columns, $whereDyn, $operator);
 //Passer la variable 'sessionAuthOk' en 'true' ou 'false' pour autoriser ou non ce qui sera chargé sur la page.
 $sessionAuthOk = $auth->testConnection($memberExist);
-
-echo $sessionAuthOk;
 
 function home()
 {
@@ -28,3 +28,5 @@ if (isset($_GET['log']) && $_GET['log'] === 'out')
 {
     $sessionAuthOk = $auth->disconnect();
 }
+echo $sessionAuthOk;
+
